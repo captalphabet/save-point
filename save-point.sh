a150cb5a87eabda5a96c365fa6771b60e6565eb0
captalphabet <155514567+captalphabet@users.noreply.github.com>
2025-02-06 00:14:53 +0000

    added some comments on setting up save-point

--- save-point.sh
+++ save-point.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## This is a test script, replace cargo r with installed binary, will need to source the script not bash it
-cargo r
+cargo r ## or save-point or <COMPILED BINARY IN PATH REF>
 
-SV_PATH='~/.config/save-point/_path.temp'
+SV_PATH='~/.config/save-point/_path.temp' ## replace the SV_PATH with the full path to the .config dir
 
 
 if [ -e ./_path.temp ]; then
   TEMP_PATH="$(cat $SV_PATH)"
-  echo "path to go to: $TEMP_PATH"
+  # echo "path to go to: $TEMP_PATH"
   rm $SV_PATH
   cd $TEMP_PATH
     
